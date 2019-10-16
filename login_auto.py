fb726b7d135b4c1fede0bc7839fc25421cd19142
aasutossh <aasutossh@gmail.com>
2019-10-16 09:23:16 +0545

    made login script with functions

--- login_auto.py
+++ login_auto.py
@@ -1,11 +1,21 @@
 from selenium import webdriver
-from selenium.webdriver.support.select import Select
 from time import sleep
+def login(firefox):
+    firefox.get("http://automationpractice.com/index.php") # go to home page
+    firefox.find_element_by_css_selector(".login").click() # click on login
 
-firefox = webdriver.Firefox()
+    firefox.find_element_by_css_selector("#email").send_keys("email@address.org")
+    firefox.find_element_by_id("passwd").send_keys("password")
+    firefox.find_element_by_css_selector("#SubmitLogin").click()
+    sleep(3)
+def successful_login(firefox):
+    return firefox.current_url == "http://automationpractice.com/index.php?controller=my-account"
 
-firefox.get("http://automationpractice.com/index.php") # go to home page
-firefox.find_element_by_css_selector(".login").click() # click on login
+if __name__ == "__main__":
+    firefox = webdriver.Firefox()
+    login(firefox)
 
-firefox.find_element_by_css_selector("#email").send_keys("email@address.org")
-firefox.find_element_by_id("passwd").send_keys("password")
+    if successful_login:
+        print("Successful login")
+    else:
+        print("Error somewhere.")
